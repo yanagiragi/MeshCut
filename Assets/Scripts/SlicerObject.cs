10f92b3f473803b8de742ef9364a429b1ff03831
yanagiragi <qaz90473@gmail.com>
2019-12-07 02:26:50 +0800

    Refactor whole project

---
+++ Assets/Scripts/SlicerObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Yr;
+
+[RequireComponent(typeof(MeshFilter))]
+
+public class SlicerObject : MonoBehaviour
+{
+    public Transform PlaneTransform;
+    public Material capMaterial;
+
+    private Yr.MeshCut _meshCutter = new Yr.MeshCut();
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // For Debug
+            gameObject.SetActive(false);
+
+            GameObject[] sliced = _meshCutter.Slice(gameObject, PlaneTransform.up, PlaneTransform.position, capMaterial);
+
+            foreach(var i in sliced)
+            {
+                i.name = string.Format("{0} ({1})", gameObject.name, i.name);
+
+                if (i.GetComponent<MeshCollider>() == null)
+                {
+                    MeshCollider meshCollider = i.AddComponent<MeshCollider>();
+                    meshCollider.sharedMesh = i.GetComponent<MeshFilter>().mesh;
+                }
+
+                if (i.GetComponent<SlicerObject>() == null) {
+                    SlicerObject slicerObject = i.AddComponent<SlicerObject>();
+                    slicerObject.PlaneTransform = this.PlaneTransform;
+                    slicerObject.capMaterial = this.capMaterial;
+                }
+            }            
+        }
+    }
+
+}
---
+++ Assets/Scripts/Yr/Plane.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yr
+{
+    public class Plane
+    {
+        private Vector3 m_Normal;
+        private float m_Distance;
+
+        public Plane()
+        {
+            m_Normal = Vector3.up;
+            m_Distance = 0.0f;
+        }
+
+        public Plane(Vector3 inNormal, Vector3 inPoint)
+        {
+            m_Normal = Vector3.Normalize(inNormal);
+            m_Distance = -Vector3.Dot(m_Normal, inPoint);
+        }
+
+        public void UpdateParam(Vector3 inNormal, Vector3 inPoint)
+        {
+            m_Normal = Vector3.Normalize(inNormal);
+            m_Distance = -Vector3.Dot(m_Normal, inPoint);
+        }
+
+        public Vector3 GetNormal()
+        {
+            return m_Normal;
+        }
+
+        public float DistFromPlane(Vector3 point)
+        {
+            return Vector3.Dot(m_Normal, point) + m_Distance;
+        }
+
+        public bool isLeftSideFromPlane(Vector3 point)
+        {
+            return (Vector3.Dot(m_Normal, point) + m_Distance) > 0.0f;
+        }
+    }
+
+}
